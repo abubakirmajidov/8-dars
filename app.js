7d7e8ac8bc60791360d48997b97a24979a0d6c1d
abubakirmajidov <abubakirmajidov@gmail.com>
2024-03-28 23:29:30 -1200

    5.8dars

---
+++ app.js
@@ -0,0 +1,44 @@
+const h3EL = document.querySelector("h3");
+const answersbtn = document.querySelectorAll(".btn");
+const restartbtn = document.querySelector(".btn1");
+const serchba = document.getElementsByClassName("serch");
+
+let a = 0;
+let isGameOver = false;
+function generateNumber(to) {
+  return Math.ceil(Math.random() * to);
+}
+
+generateNumber();
+
+function startGame() {
+  let num1 = generateNumber(100);
+  let num2 = generateNumber(100);
+  const result = num1 + num2;
+  h3EL.textContent = `${num1} + ${num2} = ?`;
+  const answers = [result - 2, result - 2, result + 1];
+
+  answers[generateNumber(3) - 1] = result;
+
+  answersbtn.forEach((btn, index) => {
+    btn.textContent = answers[index];
+
+    btn.addEventListener("click", () => {
+      if (!isGameOver) {
+        if (btn.textContent == result) {
+          alert("Tog'ri");
+        } else {
+          alert("No'tog'ri");
+        }
+
+        isGameOver = true;
+      }
+    });
+  });
+}
+
+startGame();
+
+restartbtn.addEventListener("click", startGame);
+
+serchba.textContent = a;
